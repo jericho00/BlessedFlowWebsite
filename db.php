c213d2cb64375afab1dc2f995768fa11b217e88a
jcrmarcon <jchi0319@gmail.com>
2022-02-04 21:02:13 +0800

    addedd php file

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$db_name = "login";
+
+$conn = mysqli_connect($servername, $username, $password, $db_name);
+
+if (!$conn) {
+    echo "Connection failed!";
+    die("Could not connect to MySQL server.");
+}
+else{
+    echo "Hello World";
+}
